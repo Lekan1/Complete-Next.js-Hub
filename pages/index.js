de1474542173003b02a873a051fe386647694e0c
Lekan Dar <Lekandar11@gmail.com>
2022-07-06 13:21:46 -1200

    kk

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,34 @@
-import Layout from "./layouts/Layout";
+import { BiUserPlus } from "react-icons/bi";
+import Form from "../components/Form";
+import Table from "../components/Table";
 
 export const Home = () => {
   return (
-    <Layout>
-      <h4>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sint
-        dolorem eveniet? Ipsam voluptates at corporis, unde impedit explicabo
-        aspernatur tenetur repellat quaerat, nemo dolorum vitae amet similique
-        corrupti atque quas minima perspiciatis non! Aperiam, natus. Ex totam
-        labore molestias saepe dolore voluptatem, fuga similique sint placeat,
-        omnis nulla harum dolor fugiat eos, voluptatibus at corrupti ratione
-        magnam laboriosam? Voluptatibus, veritatis dicta culpa aspernatur facere
-        iste ullam sed asperiores aliquam, reprehenderit soluta nisi neque
-        cumque maxime suscipit repudiandae tempora totam ipsum architecto?
-        Maiores temporibus autem quidem voluptates aliquam ipsam, id at hic fuga
-        asperiores laborum quisquam commodi nulla optio ipsa culpa. Vel
-        consectetur aliquid iusto laudantium nisi dolorum, soluta molestias illo
-        laboriosam saepe aperiam et, accusamus blanditiis eum suscipit eligendi
-        accusantium, fuga qui? Deleniti dicta dignissimos velit est aperiam odit
-        modi veritatis nisi illum. Omnis impedit culpa cumque quos. Rerum harum,
-        quae architecto tempora dolor, cumque dolorem voluptas, vel iure amet
-        sapiente nesciunt aliquid vero. Cupiditate aspernatur hic consequuntur
-        magnam possimus id nostrum quibusdam sed velit? Repellendus debitis
-        dicta doloribus vero labore, sed quidem modi laboriosam corrupti sit,
-        libero voluptatem veniam quaerat, optio itaque eius expedita! Amet eos
-        voluptate delectus tempore minima deserunt ab reiciendis aliquid maxime
-        dolores, vitae illo consequuntur nostrum tenetur, facere quis tempora!
-        Praesentium incidunt adipisci ea. Aperiam tenetur accusantium cupiditate
-        et nostrum id consequatur dolor nihil aspernatur, vitae provident
-        repellat adipisci incidunt commodi dolorem officia nobis minima
-        nesciunt. Maxime magni amet distinctio est, fuga necessitatibus libero
-        quos veniam qui quaerat magnam dolores cumque pariatur omnis quae
-        dolorum obcaecati illo modi esse ullam neque in eveniet ipsum. Magni aut
-        sapiente tempore error, alias, aperiam a repellat voluptatum cum itaque
-        quasi mollitia nihil illum minima saepe quibusdam dolor earum expedita
-        blanditiis reprehenderit quod repellendus. Eius voluptatibus repellendus
-        eum pariatur labore nemo iusto maiores, a natus, optio inventore
-        aliquam.
-      </h4>
-    </Layout>
+    <main className="py-5 space-y-8">
+      <h1 className="text-xl md:text-5xl text-center font-bold py-10">
+        Employee Management
+      </h1>
+
+      <div className="container mx-auto flex justify-between py-5 border-b">
+        <div className="left flex gap-3">
+          <button className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 shadow-md">
+            Add Employee{" "}
+            <span className="px-1">
+              <BiUserPlus size={23}></BiUserPlus>
+            </span>
+          </button>
+        </div>
+      </div>
+
+      {/* collapsable form */}
+      <div className="container mx-auto">
+        <Form />
+      </div>
+      {/* table */}
+      <div className="container mx-auto">
+        <Table />
+      </div>
+    </main>
   );
 };
 
