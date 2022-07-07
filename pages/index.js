ec2b512b26e59d2cf3e437c2e009116350f876a6
Lekan Dar <Lekandar11@gmail.com>
2022-07-06 21:03:03 -1200

    Submitting Form Using UseReducer

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,38 @@
-import Layout from "./layouts/Layout";
+import { useEffect, useReducer } from "react";
+
+const FormReducer = (state, action) => {
+  return {
+    ...state,
+    [event.target.name]: event.target.value,
+  };
+};
 
 export const Home = () => {
+  const [formData, setFormData] = useReducer(FormReducer, {});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <Layout>
-      <h4>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sint
-        dolorem eveniet? Ipsam voluptates at corporis, unde impedit explicabo
-        aspernatur tenetur repellat quaerat, nemo dolorum vitae amet similique
-        corrupti atque quas minima perspiciatis non! Aperiam, natus. Ex totam
-        labore molestias saepe dolore voluptatem, fuga similique sint placeat,
-        omnis nulla harum dolor fugiat eos, voluptatibus at corrupti ratione
-        magnam laboriosam? Voluptatibus, veritatis dicta culpa aspernatur facere
-        iste ullam sed asperiores aliquam, reprehenderit soluta nisi neque
-        cumque maxime suscipit repudiandae tempora totam ipsum architecto?
-        Maiores temporibus autem quidem voluptates aliquam ipsam, id at hic fuga
-        asperiores laborum quisquam commodi nulla optio ipsa culpa. Vel
-        consectetur aliquid iusto laudantium nisi dolorum, soluta molestias illo
-        laboriosam saepe aperiam et, accusamus blanditiis eum suscipit eligendi
-        accusantium, fuga qui? Deleniti dicta dignissimos velit est aperiam odit
-        modi veritatis nisi illum. Omnis impedit culpa cumque quos. Rerum harum,
-        quae architecto tempora dolor, cumque dolorem voluptas, vel iure amet
-        sapiente nesciunt aliquid vero. Cupiditate aspernatur hic consequuntur
-        magnam possimus id nostrum quibusdam sed velit? Repellendus debitis
-        dicta doloribus vero labore, sed quidem modi laboriosam corrupti sit,
-        libero voluptatem veniam quaerat, optio itaque eius expedita! Amet eos
-        voluptate delectus tempore minima deserunt ab reiciendis aliquid maxime
-        dolores, vitae illo consequuntur nostrum tenetur, facere quis tempora!
-        Praesentium incidunt adipisci ea. Aperiam tenetur accusantium cupiditate
-        et nostrum id consequatur dolor nihil aspernatur, vitae provident
-        repellat adipisci incidunt commodi dolorem officia nobis minima
-        nesciunt. Maxime magni amet distinctio est, fuga necessitatibus libero
-        quos veniam qui quaerat magnam dolores cumque pariatur omnis quae
-        dolorum obcaecati illo modi esse ullam neque in eveniet ipsum. Magni aut
-        sapiente tempore error, alias, aperiam a repellat voluptatum cum itaque
-        quasi mollitia nihil illum minima saepe quibusdam dolor earum expedita
-        blanditiis reprehenderit quod repellendus. Eius voluptatibus repellendus
-        eum pariatur labore nemo iusto maiores, a natus, optio inventore
-        aliquam.
-      </h4>
-    </Layout>
+    <div className="container">
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstName"
+          onChange={setFormData}
+          placeholder="Enter FirstName "
+        />
+        <input
+          type="text"
+          name="lastName"
+          onChange={setFormData}
+          placeholder="Enter LastName "
+        />
+        <button type="submit">Submit Form</button>
+      </form>
+    </div>
   );
 };
 
