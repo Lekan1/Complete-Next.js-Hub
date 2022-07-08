22c91495907cc9292bc80fcfbc8ebd26c2266d7f
Lekan Dar <Lekandar11@gmail.com>
2022-07-07 15:25:40 -1200

    setting up forms in the formhandling branch

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
+import Head from "next/head";
 import { BiUserPlus } from "react-icons/bi";
-import Form from "../components/Form";
-import Table from "../components/Table";
+import Table from "../components/table";
+import Form from "../components/form";
+import { useState } from "react";
 
 export const Home = () => {
+  const [visible, setVisible] = useState(false);
+
+  const handler = () => {
+    setVisible(!visible);
+  };
   return (
-    <main className="py-5 space-y-8">
+    <main className="py-5">
       <h1 className="text-xl md:text-5xl text-center font-bold py-10">
         Employee Management
       </h1>
 
       <div className="container mx-auto flex justify-between py-5 border-b">
         <div className="left flex gap-3">
-          <button className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 shadow-md">
+          <button
+            onClick={handler}
+            className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800"
+          >
             Add Employee{" "}
             <span className="px-1">
               <BiUserPlus size={23}></BiUserPlus>
@@ -21,12 +31,11 @@ export const Home = () => {
       </div>
 
       {/* collapsable form */}
-      <div className="container mx-auto">
-        <Form />
-      </div>
+      {visible ? <Form></Form> : <></>}
+
       {/* table */}
       <div className="container mx-auto">
-        <Table />
+        <Table></Table>
       </div>
     </main>
   );
